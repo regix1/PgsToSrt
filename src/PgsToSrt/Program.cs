7aa6c58e064b893553372948dc0e6195142d5ab3
Regix <red0fireus@gmail.com>
2025-07-01 17:59:16 -0500

    whoops

--- src/PgsToSrt/Program.cs
+++ src/PgsToSrt/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
+using PgsToSrt.Options;
 using System;
 using System.Reflection;
 
